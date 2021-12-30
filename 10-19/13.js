6ce98ddb5918098638c261c4ae7586d16fb7c29e
Stefan Temme <temme@objectcode.de>
2021-12-30 23:47:40 +0100

    day 13

--- 10-19/13.js
+++ 10-19/13.js
@@ -1,36 +1,78 @@
 import {testPaper, finalPaper} from "./13-data.js";
 
+const point = "\x1b[40m#\x1b[0m";
+const other = ".";
 
-const {map,foldings} = parsePaper(testPaper);
+const {map, foldings} = parsePaper(finalPaper);
 
+let step = map;
 
+foldings.forEach((f,i)=>{
+    step=foldMap(f,step);
+    if (i==0){
+        console.log("Nach ersten Schritt:",count(step));
+    }
+});
+printMap(step);
 
-printMap(map)
-console.log(foldings);
-
-function printMap(map){
-    console.log(map.map(r=>r.join("")).join("\n"));
+function foldMap({axis, index}, map) {
+    let newMap;
+    if (axis == "y") {
+        newMap = [...map.filter((r, i) => i < index).map(r=>[...r])];
+        const mirrored = map.filter((r, i) => i > index);
+        mirrored.forEach((r, i) => r.forEach((p, j) => {
+            if (p == point) {
+                newMap[index - 1 - i][j] = p;
+            }
+        }))
+    } else {
+        newMap = map.map(r => r.filter((x, i) => i < index));
+        const mirrored = map.map(r => r.filter((x, i) => i > index));
+        mirrored.forEach((r, i) => r.forEach((p, j) => {
+            if (p == point) {
+                newMap[i][index - 1 - j] = p;
+            }
+        }));
+    }
+    return newMap;
 }
 
 
-function parsePaper(paper){
-    const [pointData,foldingData] = paper.split("\n\n");
-    const points = pointData.split("\n").map(r=>r.split(",").map(x=>parseInt(x)))
-    const size = points.reduce(({x,y},[xi,yi])=>{
+function count(map) {
+    return map.reduce((a, b) => a + b.reduce((c, d) => c + (d == point ? 1 : 0), 0), 0)
+}
+
+function printMap(map) {
+    console.log(map.map(r => r.join("")).join("\n")+"\n");
+}
+
+function parseMap(pointData) {
+    const points = pointData.split("\n").map(r => r.split(",").map(x => parseInt(x)))
+    const size = points.reduce(({x, y}, [xi, yi]) => {
         return {
-            x:Math.max(x,xi),
-            y:Math.max(y,yi)
+            x: Math.max(x, xi),
+            y: Math.max(y, yi)
         }
-    } ,{x:0,y:0})
-    const map = Array.from({length:size.y+1},_=>Array.from({length:size.x+1},__=>"."));
-    points.forEach(([x,y])=>map[y][x]="#");
-    const foldings = foldingData.split("\n").map(f=>{
-        const [axis,index] = f.split(" ")[2].split("=");
+    }, {x: 0, y: 0})
+    const map = Array.from({length: size.y + 1}, _ => Array.from({length: size.x + 1}, __ => other));
+    points.forEach(([x, y]) => map[y][x] = point);
+    return map;
+}
+
+function parseFoldings(foldingData) {
+    return foldingData.split("\n").map(f => {
+        const [axis, index] = f.split(" ")[2].split("=");
         return {
             axis,
-            index:parseInt(index)
+            index: parseInt(index)
         }
     });
+}
 
-    return {map,foldings};
+function parsePaper(paper) {
+    const [pointData, foldingData] = paper.split("\n\n");
+    const map = parseMap(pointData);
+    const foldings = parseFoldings(foldingData);
+
+    return {map, foldings};
 }
